7b8bf380305fb6e1505ea1ad427cdd11379cc018
yan786362806 <786362806@qq.com>
2016-06-20 11:51:49 +0800

    1到100相加
    简单的了解了swift语言，学习了几种基本用法，swift和c++和java有很多相似的地方，学习起来不会太吃力，这个程序就是1到100相加，简
    单的循环语句，实现了这个功能

---
+++ main.swift
@@ -0,0 +1,20 @@
+//
+//  main.swift
+//  20141105044严伟2
+//
+//  Created by 20141105044y on 16/6/20.
+//  Copyright © 2016年 20141105044y. All rights reserved.
+//
+
+import Foundation
+
+var i = 0
+
+var sum = 0
+
+while i < 101{
+
+sum=sum+i
+i++
+}
+print(sum)
